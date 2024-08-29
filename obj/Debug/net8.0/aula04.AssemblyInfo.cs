036cf0ffec95c7a64ddce2bd00a0bb8130ffae52
daitxsuelen <suelendaitx@yahoo.com.br>
2024-08-29 11:03:44 -0300

    first commit

--- obj/Debug/net8.0/aula04.AssemblyInfo.cs
+++ obj/Debug/net8.0/aula04.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aula04")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8404b276e57e4798e3e74db5d97969f0a1641d21")]
 [assembly: System.Reflection.AssemblyProductAttribute("aula04")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aula04")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
